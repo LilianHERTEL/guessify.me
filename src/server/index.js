a89f32b669794c53719fc99b031f6372b4cec1c9
Thomas TSE <chun_hei.tse@etu.uca.fr>
2019-11-20 00:40:50 +0100

    creation de register

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,11 @@ const mongoose = require('mongoose') //Pris
 const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var api = require('./api');
+var apiRoute = require('./routes/lobby.js');
+var loginRoute = require('./routes/login.js');
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var launchDataBase = require('./database.js');
 
@@ -20,13 +24,43 @@ global.roomList = {};
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 
 app.set('trust proxy', 1) // trust first proxy (autorise l'utilisation d'un proxy)
-
+app.use(express.json());
 
 // In production we need to pass these values in instead of relying on webpack
 //Pris 
 launchDataBase(app);
 
-app.use('/api', api);
+passport.serializeUser(function(user, done) {
+  user.password = null
+  
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    var User = mongoose.model('User');
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      user.comparePassword(password,(err,isMatch) => {
+        if(err) done(null, false, { message: err })
+        if(!isMatch) done(null, false, { message: 'Incorrect password.' })
+        done(null, user);
+    })
+    });
+  }
+));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/api', apiRoute);
+app.use('/auth', loginRoute);
+
 // get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
---
+++ src/server/routes/login.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const uniqid = require('uniqid');
+var router = express.Router();
+const mongoose = require('mongoose');
+var passport = require('passport')
+
+router.post('/login',passport.authenticate('local'), function(req, res){
+    
+  
+res.json({success:true,msg:req.user});
+  });
+
+router.post('/register' , function(req, res){
+    var data = req.body;
+  var User = mongoose.model('User');
+  var newUser = new User({
+      username: data.username,
+      password: data.password
+  })
+  newUser.save(function (err){
+    if (err) return console.error(err);
+    console.log("User saved")
+  })
+res.json({success:true,msg:"You have successfully registered!"});
+
+});
+
+
+
+module.exports = router;
