7afb07c3db7fc9785209683e04a881eae2018b0e
Hector Piteau <hector.piteau@gmail.com>
2019-11-19 23:34:59 +0100

    externalisation de la fonction de lancement de BD

---
+++ src/server/database.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+/* eslint consistent-return:0 */
+var initDatabase = require('./Schema');
+var session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+
+function launchDataBase(app){
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb://guessify:pAI5v2#NQk#W@kimsufi.thomasxd24.com/guessify', {useNewUrlParser: true,useUnifiedTopology: true});
+
+    var db = mongoose.connection;
+			app.use(session({
+			secret: 'je reflechis je prends mon temps',
+			resave: false,
+			saveUninitialized: true,
+			store: new MongoStore({ mongooseConnection: db }),
+			cookie: { secure: false }
+    }))
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+    initDatabase();
+    var User = mongoose.model('User');
+    });
+}
+
+module.exports = launchDataBase;
--- src/server/index.js
+++ src/server/index.js
@@ -8,36 +8,25 @@ const logger = require('./util//logger');
 const argv = require('./util/argv');
 const port = require('./util//port');
 const websocket = require('./websocket');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') //Pris
 const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var api = require('./api');
-var initDatabase = require('./Schema')
+
+var launchDataBase = require('./database.js');
+
 global.roomList = {};
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 
-var session = require('express-session')
-const MongoStore = require('connect-mongo')(session);
-app.set('trust proxy', 1) // trust first proxy
+app.set('trust proxy', 1) // trust first proxy (autorise l'utilisation d'un proxy)
 
-mongoose.connect('mongodb://guessify:pAI5v2#NQk#W@10.8.0.1/guessify', {useNewUrlParser: true,useUnifiedTopology: true});
 
 // In production we need to pass these values in instead of relying on webpack
-var db = mongoose.connection;
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore({ mongooseConnection: db }),
-  cookie: { secure: false }
-}))
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  initDatabase();
-  var User = mongoose.model('User');
-});
-app.use('/api', api)
+//Pris 
+launchDataBase(app);
+
+app.use('/api', api);
 // get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
