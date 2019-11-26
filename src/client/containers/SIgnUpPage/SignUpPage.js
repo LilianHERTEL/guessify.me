268986c2e3846aecad15eae9f6135784bb755441
Lilian Hertel <lilian.hertel@etu.uca.fr>
2019-11-26 13:08:55 +0100

    Ajout d'un event handler pour le bouton Sign Up

--- src/client/containers/SIgnUpPage/SignUpPage.js
+++ src/client/containers/SIgnUpPage/SignUpPage.js
@@ -16,13 +16,13 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 import {Link as RouterLink} from 'react-router-dom';
 import color from '@material-ui/core/colors/purple';
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   '@global': {
     body: {
       backgroundColor: theme.palette.common.white,
@@ -49,102 +49,143 @@ const useStyles = makeStyles(theme => ({
     padding: '20px',
     backgroundColor: theme.palette.common.white,
   },
-}));
+});
 
-export default function SignUpPage() {
-  const classes = useStyles();
+class SignUpPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password:   '',
+      confirmPassword: '',
+      email: '',
+      tosAgreement: ''
+    }
+    this.onAnyInputChange = this.onAnyInputChange.bind(this);
+    this.onSignUpButtonClick = this.onSignUpButtonClick.bind(this);
+  }
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Grid item>
-          <img id="banner" title="This is our awesome banner ! Cool huh ?" src="/src/client/images/bannière.png" />
-        </Grid>
-        <Box border={1} borderColor="primary.main" borderRadius={20} className={classes.box}>
-            <Typography component="h1" variant="h5" align="center" color="primary">
-            Sign up
-            </Typography>
-            <form className={classes.form} noValidate>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="UserName"
-                    name="lastName"
-                    autoComplete="lname"
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    name="email"
-                    autoComplete="email"
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    name="confirm-password"
-                    label="Confirm Password"
-                    type="password"
-                    id="confirm-password"
-                    autoComplete="current-password"
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <FormControlLabel
-                    control={<Checkbox value="agreementToS" color="primary" />}
-                    label={
-                        <React.Fragment>
-                            <span>I agree with </span>
-                            <Link href="#" variant="body1">
-                                Terms and Conditions
-                            </Link>
-                        </React.Fragment>
-                    }
-                />
-                </Grid>
-            </Grid>
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-            >
-                Sign Up
-            </Button>
-            <Grid container justify="flex-end">
-                <Grid item>
-                <Link href="/" variant="body2">
-                    You already have an account ? Log in
-                </Link>
-                </Grid>
-            </Grid>
-            </form>
-        </Box>
-      </div>
-    </Container>
-  );
+  onAnyInputChange(event){
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  onSignUpButtonClick(event) {
+    event.preventDefault();
+    const user = {
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email
+    }
+    
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Grid item>
+            <img id="banner" title="This is our awesome banner ! Cool huh ?" src="/src/client/images/bannière.png" />
+          </Grid>
+          <Box border={1} borderColor="primary.main" borderRadius={20} className={classes.box}>
+              <Typography component="h1" variant="h5" align="center" color="primary">
+              Sign up
+              </Typography>
+              <form className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                  <TextField
+                      value={this.state.username}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="userName"
+                      label="UserName"
+                      name="username"
+                      autoComplete="lname"
+                      onChange={this.onAnyInputChange}
+                  />
+                  </Grid>
+                  <Grid item xs={12}>
+                  <TextField
+                      value={this.state.email}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email"
+                      name="email"
+                      autoComplete="email"
+                      onChange={this.onAnyInputChange}
+                  />
+                  </Grid>
+                  <Grid item xs={12}>
+                  <TextField
+                      value={this.state.password}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      onChange={this.onAnyInputChange}
+                  />
+                  </Grid>
+                  <Grid item xs={12}>
+                  <TextField
+                      value={this.state.confirmPassword}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      type="password"
+                      id="confirm-password"
+                      autoComplete="current-password"
+                      onChange={this.onAnyInputChange}
+                  />
+                  </Grid>
+                  <Grid item xs={12}>
+                  <FormControlLabel
+                      name="tosAgreement" 
+                      control={<Checkbox value="agreementToS" color="primary" />}
+                      label={
+                          <React.Fragment>
+                              <span>I agree with </span>
+                              <Link href="#" variant="body1">
+                                  Terms and Conditions
+                              </Link>
+                          </React.Fragment>
+                      }
+                      onChange={this.onAnyInputChange}
+                  />
+                  </Grid>
+              </Grid>
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={this.onSignUpButtonClick}
+              >
+                  Sign Up
+              </Button>
+              <Grid container justify="flex-end">
+                  <Grid item>
+                  <Link href="/" variant="body2">
+                      You already have an account ? Log in
+                  </Link>
+                  </Grid>
+              </Grid>
+              </form>
+          </Box>
+        </div>
+      </Container>
+    );
+  }
 }
+
+export default withStyles(styles)(SignUpPage);
