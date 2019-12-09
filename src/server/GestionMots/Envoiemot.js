0b6329afe699c0d17bfcfe3ea15e967db4ab85eb
robarou <Romain.barou@etu.uca.fr>
2019-12-09 10:13:43 +0100

    conversion tirage en fonction

--- src/server/GestionMots/Envoiemot.js
+++ src/server/GestionMots/Envoiemot.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var dictionnaire;
 var tabmot;
 
-fs.readFile('../Dictionnaires/ENdic.txt',function(err,data){
+fs.readFile('src/server/Dictionnaires/ENdic.txt',function(err,data){
     if(err) throw err;
     var dictionnaire = data.toString().split("\n");
     for (i in tab){
@@ -10,11 +10,18 @@ fs.readFile('../Dictionnaires/ENdic.txt',function(err,data){
     }
 });
 
-var tabmot=[];
-
-for (let i=0; i<3;i++){
-    let random;
-    random=Math.floor(Math.random() * Math.floor(dictionnaire.length));
-    tabmot.push(dictionnaire[random]);
+function tirerMots(dictionnaire){
+    var tabmot=[];
+    var indexs=[];
+    for (let i=0; i<3;i++){
+        let random;
+        random=Math.floor(Math.random() * Math.floor(dictionnaire.length));
+        while(indexs.indexOf(random)!=-1){
+            random=Math.floor(Math.random() * Math.floor(dictionnaire.length));
+        }
+        indexs.push(random);
+        tabmot.push(dictionnaire[random]);
+    }
+    return tabmot;
 }
 console.log(tabmot);
