3b9449a99800bf6d6c425353ee91b1586c0c3d84
thomasxd24 <thomasxd24@hotmail.com>
2019-12-09 11:04:14 +0100

    added socket id,

--- src/server/websocket/index.js
+++ src/server/websocket/index.js
@@ -3,10 +3,8 @@ function start(io) {
   console.log("bitchstart")
     io.on('connection', function(socket){
       socket.isInGame = false;
-      console.log(socket.request.session.id)
       socket.on('findGame', async function(username){
         var Lobby = mongoose.model("Lobby");
-        
         var lobbyResult = await Lobby.aggregate([{$project: {
           cmp_value: {$cmp: ['$maxPlayer', { $size: '$listPlayer' }]}
       }},
@@ -21,7 +19,7 @@ function start(io) {
       }
         socket.username = username;
         socket.lobby = lobby
-        await lobby.join(socket.request.session.id,username);
+        await lobby.join(socket.id,username);
         socket.join(lobby._id.toString());
         socket.isInGame = true;
         socket.emit("joinedGame",
@@ -37,6 +35,10 @@ function start(io) {
         socket.on('draw', function(msg){
           if(!socket.isInGame) return socket.emit("Unauthorized","You are not allowed send this command!");
           io.to(socket.lobby._id).emit('drawCmd', msg);
+        });
+        socket.on('requestListPlayer', function(msg){
+          if(!socket.isInGame) return socket.emit("Unauthorized","You are not allowed send this command!");
+          io.to(socket.lobby._id).emit('listPlayer', socket.lobby);
         });
       });
       console.log("inited")
