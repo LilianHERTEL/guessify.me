6f2f41216e9203766507b0832daf4845533d4fe7
Thomas Tse <chun_hei.tse@etu.uca.fr>
2020-01-04 00:04:35 +0100

    try fix

--- src/client/containers/GamePage/GamePage.js
+++ src/client/containers/GamePage/GamePage.js
@@ -241,7 +241,7 @@ const GamePage = (props) => {
   useEffect(() => {
     if (!props.location.state) return;
     if(window.location.hostname == "guessify.me")
-    socket = openSocket('http://kimsufi.thomasxd24.com:8880/');
+    socket = openSocket('https://ws.guessify.me:8880/');
     else
     socket = openSocket('http://'+window.location.hostname+':8880/');
     connect(props.location.state.username);
