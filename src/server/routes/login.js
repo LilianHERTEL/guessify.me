20ff9b95589c2c22e2ada794f41804311a5ed971
robarou <Romain.barou@etu.uca.fr>
2020-01-06 08:10:16 +0100

    insertion des modules nodemailer et express mailer

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -3,6 +3,19 @@ const uniqid = require('uniqid');
 var router = express.Router();
 const mongoose = require('mongoose');
 var passport = require('passport')
+const mailer = require('express-mailer');
+
+mailer.extend(app, {
+  from: 'no-reply@example.com',
+  host: 'smtp.gmail.com', // hostname
+  secureConnection: true, // use SSL
+  port: 465, // port for secure SMTP
+  transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+  auth: {
+    user: 'gmail.user@gmail.com',
+    pass: 'userpass'
+  }
+});
 
 router.post('/login',passport.authenticate('local'), function(req, res){
   //assigne l'utilisateur à sa session
@@ -29,8 +42,24 @@ router.post('/unregister',passport.authenticate('local'),function(req,res){
   Permet de s'inscrire en tant que membre sur le site. Cette fonction va enregistrer le nouvel utilisateur dans la base de données.
 */
 router.post('/register' , function(req, res){
-    var data = req.body;
+  var data = req.body;
   var User = mongoose.model('User');
+
+  app.mailer.send('email', {
+    to: 'atomicadx@gmail.com', // REQUIRED. This can be a comma delimited string just like a normal email to field. 
+    subject: 'Test Email', // REQUIRED.
+    otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
+  }, function (err) {
+    if (err) {
+      // handle error
+      console.log(err);
+      res.send('There was an error sending the email');
+      return;
+    }
+    res.send('Email Sent');
+  });
+
+
   var newUser = new User({
       ...data
   })
@@ -38,8 +67,6 @@ router.post('/register' , function(req, res){
     if (err) return res.status(401).json({success:false,msg:err.errmsg});
     return res.json({success:true,msg:"You have successfully registered!"});
   })
-
-
 });
 
 router.get('/logout', function(req,res){
