caad371c31c6508aafa0bce2f538c0ad50ab8017
Lilian <lilian.hertel@etu.uca.fr>
2020-02-04 22:21:07 +0100

    fixed word display for - dashes and white spaces

--- src/server/GestionMots/Envoiemot.js
+++ src/server/GestionMots/Envoiemot.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 var dictionnaire = [];
 
-fs.readFile(__dirname+'/../Dictionnaires/fr-FR.txt','utf8',function(err,data){
-    if(err) throw err;
+fs.readFile(__dirname + '/../Dictionnaires/fr-FR.txt', 'utf8', function (err, data) {
+    if (err) throw err;
     dictionnaire = data.toString().split("\r\n");
 });
 
-function tirerMots(){
-    random=Math.floor(Math.random() * Math.floor(dictionnaire.length))
+function tirerMots() {
+    random = Math.floor(Math.random() * Math.floor(dictionnaire.length))
     return dictionnaire[random];
 }
 
 function underscoreWordToBeDrawn(word) {
+    if ((typeof word) !== 'string') return "";
     let underscored_word = "";
+    let currLetter = "";
     for (let i = 0; i < word.length; i++) {
-      underscored_word += "_ ";
+        currLetter = word.charAt(i);
+        currLetter === "-" || currLetter === " " ? underscored_word += currLetter + "\xa0" : underscored_word += "_\xa0"; // \xa0 stands for a no-break space
     }
     return underscored_word;
 }
