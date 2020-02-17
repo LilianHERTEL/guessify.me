8c2df9f2c694417f0d09a0aa470e86408394d18a
Hector Piteau <hector.piteau@gmail.com>
2020-02-17 10:01:02 +0100

    creation renderArea v3

---
+++ src/client/containers/GamePage/RenderAreaV3.js
@@ -0,0 +1,55 @@
+import React, { useRef, useEffect, useReducer } from 'react';
+import './style.css';
+import { Paper, Grid, Box, Container, LinearProgress, Typography, AppBar, Tabs, Tab, Divider, Switch, TextField, ListItemSecondaryAction } from '@material-ui/core';
+
+import CanvasDraw from "react-canvas-draw";
+
+
+const RenderAreaV2 = (props) => {
+    const drawingZoneRef = React.useRef(null);
+    /****************************************************************************/
+    /****************************************************************************/
+    /* SVG AREA SIZING UTILS
+     *
+     * Used to initialize and update responsive svg area size (height)
+     */
+    const [svgBoxHeight, setSvgBoxHeight] = React.useState(0);
+    const [svgBoxWidth, setSvgBoxWidth] = React.useState(0);
+    /**
+     * (Hook version of "componentDidMount" lifecycle method)
+     * **
+     * This effect is executed only once : after the component has mounted
+     * **
+     * Sets the initial drawing area size
+     */
+    React.useEffect(() => {
+        console.log("DrawingRenderArea MOUNTED");
+        // Sets the initial drawing area size
+        const w = document.getElementById('svgArea').clientWidth;
+        const h = w / 1060 * 582;
+        setSvgBoxHeight(h);
+        setSvgBoxWidth(w);
+    }, []);
+
+    /**
+     * Updates drawing area size on resize event
+     */
+    window.onresize = () => {
+        const newWidth = document.getElementById('svgArea').clientWidth;
+        const newHeight = newWidth / 1060 * 582;
+        setSvgBoxHeight(newHeight);
+        setSvgBoxWidth(newWidth);
+    }
+    /****************************************************************************/
+
+
+    return (
+        <Box height={svgBoxHeight}>
+            <Paper>
+                <CanvasDraw disabled={true} saveData={props.listPath} ref={canvasDraw => (drawingZoneRef.current = canvasDraw)} id="canvas-id" className="drawingRenderArea" canvasWidth={svgBoxWidth} canvasHeight={svgBoxHeight} />
+            </Paper>
+        </Box>
+    );
+}
+
+export default RenderAreaV2;
