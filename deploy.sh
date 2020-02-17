34fb22ee136e6afcb4a0978ade02362e03c2bebf
thomasxd24 <thomasxd24@hotmail.com>
2020-02-17 11:54:45 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME=Guessify
-PM2_NAME=Guessify
+PM2_NAME=guessify.me
 discord_url=https://discordapp.com/api/webhooks/676356429208092672/TM49ti4Q7nl6kJKVS9hwIIp1rC7Fk1046yUeizGRX4NaO3CS0QeCn_CDK7rT-qg2Iaqd
 
 curl -X POST -H "Content-Type: multipart/form-data" -F "content=$PROJECT_NAME - Deploy starting, app will be restarted" "$discord_url"
@@ -23,4 +23,4 @@ curl -X POST -H "Content-Type: multipart/form-data" -F "content=$PROJECT_NAME - 
 pm2 reload $PM2_NAME --update-env
 
 
-curl -X POST -H "Content-Type: multipart/form-data" -F "content=$PROJECT_NAME - Deploy finished" "$discord_url"
+curl -X POST -H "Content-Type: multipart/form-data" -F "content=$PROJECT_NAME - Deploy finished @everyone Guessify.me restarted" "$discord_url"
